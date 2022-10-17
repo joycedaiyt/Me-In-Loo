25891de8a3e507dfc0964adb5a0ec3cc16ceed1b
Feng-12138 <yiransun068@gmail.com>
2022-10-16 21:42:33 -0400

    delete ssl

--- database_setup/gcp_mysql_connection.py
+++ database_setup/gcp_mysql_connection.py
@@ -4,11 +4,7 @@ from mysql.connector.constants import ClientFlag
 config = {
     'user': 'me-in-loo',
     'password': 'uwaterloo348',
-    'host': '35.193.141.203',
-    'client_flags': [ClientFlag.SSL],
-    'ssl_ca': 'ssl/server-ca.pem',
-    'ssl_cert': 'ssl/client-cert.pem',
-    'ssl_key': 'ssl/client-key.pem'
+    'host': '35.193.141.203'
 }
 
 # now we establish our connection
