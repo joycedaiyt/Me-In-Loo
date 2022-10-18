867b71d5150daffd54f7291abe210b08ef9603a4
Feng-12138 <yiransun068@gmail.com>
2022-10-18 00:50:16 -0400

    test bucket

--- back_end/gcpbucket_setup/bucketconfig.py
+++ back_end/gcpbucket_setup/bucketconfig.py
@@ -3,7 +3,12 @@ from os import listdir
 from os.path import isfile, join
 
 # Google Cloud Storage Config
-storage_client = storage.Client()
+# storage_client = storage.Client()
+
+storage_client = storage.Client.from_service_account_json(
+    "dynamic-branch-308302-f0b255f61755.json", project="dynamic-branch-308302"
+)
+
 bucketName = "me-in-loo-bucket"
 bucket = storage_client.get_bucket(bucketName)
 
@@ -15,7 +20,7 @@ bucketFolder = 'test_folder'
 def upload_files_from_local():
     """Upload files to GCP bucket."""
 
-    # for each file in the localFolder, it'll be uploaded to the GCP bucket
+    # for each file in the localFolder (image_folder), it'll be uploaded to the GCP bucket
     # files that already exist in the bucket will not be uploaded again
 
     # image urls in the bucket
